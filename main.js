5a11fd0921baa3623485512d57b933bafb39f882
blakel58 <44421670+blakel58@users.noreply.github.com>
2023-10-15 15:32:06 -0400

    Separated scene setup from main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// Uncomment when loading a GLTF file for the drone, and remember to add const loader = new GLTFLoader();
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import renderGUI from './modules/gui';
 import helpers from './modules/helpers';
 import controls from './modules/controls';
 import {show_animation, initializeTrajectory} from './modules/show_animation';
 import ParseSkyc from './modules/parse';
+
+// Scene initialization
+import { initializeScene } from './modules/sceneSetup';
 
 // Keyboard controls
 import { moveState, initKeyboardControls } from './modules/keyboardControls';
@@ -32,67 +37,13 @@ renderer.shadowMap.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const light = new THREE.AmbientLight(0x333333, 4);
-const Dlight = new THREE.DirectionalLight(0xFFFFFF, 1);
-Dlight.position.set(200, 200, 200);
-Dlight.castShadow = true;
-Dlight.shadow.camera.top = 50;
-Dlight.shadow.camera.bottom = -50;
-Dlight.shadow.camera.right = 50;
-Dlight.shadow.camera.left = -50;
-scene.add( light );
-scene.add(Dlight);
-
-const stadiumGeo = new THREE.PlaneGeometry(100, 100);
-const stadiumMat = new THREE.MeshStandardMaterial({color: 'white', side: THREE.DoubleSide});
-const stadiumMesh = new THREE.Mesh(stadiumGeo, stadiumMat);
-scene.add( stadiumMesh );
-stadiumMesh.receiveShadow = true;
-stadiumMesh.rotation.x = .5 * Math.PI;
-
-stadiumMesh.position.set(-200, -5, -200);
-
-const fieldGeo = new THREE.PlaneGeometry(200, 200);
-const fieldMat = new THREE.MeshStandardMaterial({color: 'lightgreen', side: THREE.DoubleSide});
-const fieldMesh = new THREE.Mesh(fieldGeo, fieldMat);
-scene.add( fieldMesh );
-fieldMesh.receiveShadow = true;
-fieldMesh.rotation.x = .5 * Math.PI;
-
-fieldMesh.position.y = -1;
-
 const sphere = new THREE.SphereGeometry(1, 15, 10);
 const material = new THREE.MeshStandardMaterial({color: '#cf1657'});
 const drone = new THREE.Mesh(sphere, material);
 drone.castShadow = true;
 
-const loader = new GLTFLoader();
-
-let arena;
-loader.load( './models/arena.glb', ( glb ) => {
-	arena = glb.scene;
-
-	arena.traverse((child) => {
-		if (child.isMesh) {
-			// disabling cast shadows for now as it casts shadows onto itself in a wierd manner. you can see this through the lines that appera on it.
-			// child.castShadow = true;
-			child.receiveShadow = true;
-		}
-	});
-
-	arena.position.set(-210, 15, -250);
-	arena.rotation.x = .10;
-	arena.rotation.y = 0.15;
-	arena.rotation.z = -.03;
-	arena.scale.set(200, 200, 200);
-
-	scene.add( arena );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
+// Initialize the scene
+const { Dlight } = initializeScene(scene);
 
 // List of files
 let fileList = [];
