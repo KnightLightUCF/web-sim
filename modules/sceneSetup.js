462e11b41764b32535a1a90681b3e4e0e37a4297
Blake <44421670+blakel58@users.noreply.github.com>
2024-02-24 23:34:22 -0500

    Removed unused variable

--- modules/sceneSetup.js
+++ modules/sceneSetup.js
@@ -2,8 +2,6 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 export function initializeScene(scene, config) {
-	let droneTakeoffField;
-
 	// Light setup from config
 	const ambientLightConfig = config.light.ambient;
 	const ambientLight = new THREE.AmbientLight(ambientLightConfig.color, ambientLightConfig.intensity);
@@ -12,16 +10,15 @@ export function initializeScene(scene, config) {
 	const directionalLightConfig = config.light.directional;
 	const Dlight = new THREE.DirectionalLight(directionalLightConfig.color, directionalLightConfig.intensity);
 	Dlight.position.set(...directionalLightConfig.position);
-	Dlight.castShadow = true; // Assuming all directional lights cast shadows
+	Dlight.castShadow = true; // Assumes all directional lights cast shadows
 	
-	// Apply shadow camera settings from the configuration
+	// Apply shadow camera settings from config
 	const shadowConfig = directionalLightConfig.shadow.camera;
 	Dlight.shadow.camera.top = shadowConfig.top;
 	Dlight.shadow.camera.bottom = shadowConfig.bottom;
 	Dlight.shadow.camera.right = shadowConfig.right;
 	Dlight.shadow.camera.left = shadowConfig.left;
 	
-	// Configure other shadow properties if needed
 	scene.add(Dlight);
 
 	// helpers.js
